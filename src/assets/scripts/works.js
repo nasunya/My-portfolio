0edba4d1d3ec189e0013a4547bd04c397e697154
unknown <tischina-nast2012@yandex.ru>
2019-02-02 11:57:16 +0300

    Верстка сайта портфолио

---
+++ src/assets/scripts/works.js
@@ -0,0 +1,6 @@
+//import "./modules/blur";
+//import "./modules/form";
+//import "./modules/hamburger";
+//import "./modules/parallax-intro";
+//import "./modules/preloader";
+//import "./modules/welcome";
